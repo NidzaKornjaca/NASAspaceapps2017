2dade8a23e197af7a730b28aa0c18f74ddaa71cc
Nikola Vlahovic <nikola.vlahovic2401@gmail.com>
2017-04-29 21:18:24 +0200

    Plotting

--- solarRadiationPredictor.py
+++ solarRadiationPredictor.py
@@ -5,10 +5,31 @@ from sklearn.metrics import classification_report
 from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
 import sklearn.metrics as met
+import matplotlib.pyplot as plt
 
-def class_infor(clf, y_test, y_pred):
-	print(clf)
-	
+
+def create_predictor(x_pre, y, lag, test_size):
+	shifted = pd.concat([y, y.shift(-lag), y.shift(-2*lag), y.shift(-3*lag)], axis=1).dropna()
+	shifted.columns = ["solarRadiation", "solarRadiation-"+str(lag), "solarRadiation-"+str(2*lag), "solarRadiation-"+str(3*lag)]
+
+	y_lag_transposed = shifted.T[1:]
+	y_lag=DataFrame(y_lag_transposed.T)
+	y_transposed = shifted["solarRadiation"]
+	x = pd.concat([x_pre, y_lag], axis = 1).dropna()
+	x_train, x_test, y_train, y_test = train_test_split(x, y_transposed, test_size = test_size)
+	lr = LinearRegression()
+	lr.fit(x_train, y_train)
+	return lr, x_train, x_test, y_train, y_test
+
+def predictor_stats(x_pre, y, lag, test_size):
+	print("Lag is ", lag)
+	lr, x_train, x_test, y_train, y_test = create_predictor(x_pre, y, lag, test_size)
+	print("Test set:")
+	print(lr.score(x_test, y_test))
+	print("Training set:")
+	print(lr.score(x_train, y_train))
+	print()
+
 
 df = pd.read_csv("SpaceAppsData.csv")
 
@@ -16,28 +37,22 @@ features = df.columns[:9].tolist()
 features.remove("date")
 features.remove("localTime")
 features.remove("row ID")
-features.remove("time")
 features.remove("windDirection")
 
+#features = ["time"]
 
 y = DataFrame(df["solarRadiation"])
 
 x_pre = df[features]
 
-shifted = pd.concat([y, y.shift(-5), y.shift(-10), y.shift(-15)], axis=1).dropna()
-shifted.columns = ["solarRadiation", "solarRadiation-1", "solarRadiation-2", "solarRadiation-3"]
+for i in range(10, 30):
+	predictor_stats(x_pre, y, i, 0.5)
 
-x_pre = df[features]
-y_lag_transposed = shifted.T[1:]
-y_lag=DataFrame(y_lag_transposed.T)
-y_transposed = shifted["solarRadiation"]
-
-
-x = pd.concat([x_pre, y_lag], axis = 1).dropna()
-
-x_train, x_test, y_train, y_test = train_test_split(x, y_transposed, test_size = 0.5)
-
-lr = LinearRegression()
-lr.fit(x_train, y_train)
-print(lr.score(x_test, y_test))
-print(lr.score(x_train, y_train))
+predictor, x_train, x_test, y_train, y_test = create_predictor(x_pre, y, 30, 0.5)
+y_predicted = predictor.predict(x_test)
+print(y_predicted)
+time_test = x_test["time"]
+zipped = zip(time_test, y_test, y_predicted)
+x_axis, y_test_sorted, y_pred_sorted = zip(*sorted(zipped))
+plt.plot(x_axis, y_test_sorted, 'b-',x_axis, y_pred_sorted, 'r-')
+plt.show()
