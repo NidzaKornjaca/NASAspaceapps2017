cc671db0f846efeb7e03c77218161c5d7f1dd0ef
Nikola Vlahovic <nikola.vlahovic2401@gmail.com>
2017-04-30 11:08:39 +0200

    time

--- solarRadiationPredictor.py
+++ solarRadiationPredictor.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 from pandas import DataFrame
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -6,6 +7,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
 import sklearn.metrics as met
 import matplotlib.pyplot as plt
+import datetime
 
 
 def create_predictor(x_pre, y, lag, test_size):
@@ -16,6 +18,7 @@ def create_predictor(x_pre, y, lag, test_size):
 	y_lag=DataFrame(y_lag_transposed.T)
 	y_transposed = shifted["solarRadiation"]
 	x = pd.concat([x_pre, y_lag], axis = 1).dropna()
+	df_len = len(df.index)
 	x_train, x_test, y_train, y_test = train_test_split(x, y_transposed, test_size = test_size)
 	lr = LinearRegression()
 	lr.fit(x_train, y_train)
@@ -30,16 +33,25 @@ def predictor_stats(x_pre, y, lag, test_size):
 	print(lr.score(x_train, y_train))
 	print()
 
+def date_to_float(x):
+	return (x.hour*100 + x.minute-1200)/1200
 
-df = pd.read_csv("SpaceAppsData.csv")
+def date_to_float_array(d):
+	f = [date_to_float(x) for x in d]
+	return f
+
+
+df = pd.read_csv("SpaceAppsData.csv", parse_dates = ['localTime'])
+print(df.head())
+df["localTime"] = date_to_float_array(df["localTime"])
 
 features = df.columns[:9].tolist()
 features.remove("date")
-features.remove("localTime")
+#features.remove("localTime")
 features.remove("row ID")
 features.remove("windDirection")
 
-#features = ["time"]
+features = ["time", "localTime"]
 
 y = DataFrame(df["solarRadiation"])
 
@@ -48,11 +60,31 @@ x_pre = df[features]
 for i in range(10, 30):
 	predictor_stats(x_pre, y, i, 0.5)
 
-predictor, x_train, x_test, y_train, y_test = create_predictor(x_pre, y, 30, 0.5)
+predictor, x_train, x_test, y_train, y_test = create_predictor(x_pre, y, 10, 0.5)
 y_predicted = predictor.predict(x_test)
 print(y_predicted)
 time_test = x_test["time"]
 zipped = zip(time_test, y_test, y_predicted)
 x_axis, y_test_sorted, y_pred_sorted = zip(*sorted(zipped))
+x_axis = [datetime.datetime.fromtimestamp(x) for x in x_axis]
 plt.plot(x_axis, y_test_sorted, 'b-',x_axis, y_pred_sorted, 'r-')
+plt.show()
+
+curr_time = 1475315718
+
+data_set = DataFrame([curr_time, date_to_float(datetime.datetime.fromtimestamp(curr_time)),1.27, 1.25, 1.25]).T
+data_point = data_set
+predictions = []
+time_list = []
+
+for x in range(1, 5000):
+	fdp_prediction = predictor.predict(data_point)
+	data_point_time = x*300+curr_time
+	date_obj = datetime.datetime.fromtimestamp(data_point_time)
+	data_point = DataFrame([data_point_time, date_to_float(datetime.datetime.fromtimestamp(data_point_time)),fdp_prediction, data_point[1], data_point[2]]).T
+	data_set.append(data_point)
+	predictions.append(fdp_prediction.tolist())
+	time_list.append(date_obj)
+
+plt.plot(time_list, predictions, 'ro')
 plt.show()
